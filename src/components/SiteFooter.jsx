df898e2c8e6a45d4036d0b6de7a6c2ea420ad759
Robin Westerback <robinwesterback1989@gmail.com>
2020-07-29 17:20:41 +0000

    Add styling to the footer

--- src/components/SiteFooter.jsx
+++ src/components/SiteFooter.jsx
@@ -5,19 +5,25 @@ export default function SiteFooter() {
     return (
         <footer>
             <div>
-                <h3>Megapixel Group ROI Calculator</h3>
+                <h3>Megapixel Group
+               <br></br>
+                ROI Calculator</h3>
             </div>
-            <h4>Stockholm, Sweden</h4>
-            <h4>+46 8 189 880</h4>
-            <br></br>
-            <h4>Marbella, Spain</h4>
-            <h4>info@megapixelab.se</h4>
-            <h6>
-                Did you know that Megapixel is offering marketing services?
-              <br></br>
-                <a href="https://www.megapixelab.se/" target="_blank">https://www.megapixelab.se/</a>
-            </h6>
+
+            <div className="background-black">
+                <h4>Stockholm, Sweden</h4>
+                <h4>+46 8 189 880</h4>
+                <br></br>
+                <h4>Marbella, Spain</h4>
+                <h4>info@megapixelab.se</h4>
+            </div>
+
             <div>
+                <h6>
+                    Did you know that Megapixel is offering marketing services?
+              <br></br>
+                    <a href="https://www.megapixelab.se/" target="_blank">https://www.megapixelab.se/</a>
+                </h6>
             </div>
             <nav>
                 <p>copyright</p>
