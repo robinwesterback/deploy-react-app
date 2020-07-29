822d69a600a6f5a7859eb8f51c25297f9cb00501
Tarzan <robinwesterback1989@gmail.com>
2020-07-29 14:39:57 +0200

    Updates

---
+++ precache-manifest.c9e27c2095e4388bffad3671ca534ebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a425657794a3c2da61ffe17d92797c4a",
+    "url": "/deploy-react-app/index.html"
+  },
+  {
+    "revision": "e5acc324727c3f772f63",
+    "url": "/deploy-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5b0b123ab9d144cfe19c",
+    "url": "/deploy-react-app/static/js/2.0352da08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react-app/static/js/2.0352da08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5acc324727c3f772f63",
+    "url": "/deploy-react-app/static/js/main.7758e54b.chunk.js"
+  },
+  {
+    "revision": "de85756a32defe5e3eb4",
+    "url": "/deploy-react-app/static/js/runtime-main.bfaf240e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
