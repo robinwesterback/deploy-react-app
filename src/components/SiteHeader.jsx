2a0e00453371e362a2d103f7355127ef71c96bcd
Robin Westerback <robinwesterback1989@gmail.com>
2020-07-30 14:57:45 +0000

    Add logo and fontawesome bars to the header

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import logo from '../assets/images/megapixel-logo-tiny.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-export default function SiteHeader({logoName = "Logo"}) {
+
+export default function SiteHeader() {
 
     return (
         <header className="header">
-          <a className="logo">{logoName}</a>
-          
-          <nav className="navbar">
-              <a href="" target="_blank">Home</a>
-              <a href="" target="_blank">Business</a>
-              <a href="" target="_blank">Campaign</a>
-              <a href="" target="_blank">ROI</a>
-              <a href="" target="_blank">Support</a>
+          <a className="logo">
+            <img src={logo} alt="Logo"></img>
+          </a>
+
+          <nav>
+            <button><FontAwesomeIcon icon={faBars} /></button>
+            <a href="#home">Home</a>
+            <a href="#business">Business</a>
+            <a href="campaign">Campaign</a>
+            <a href="roi">ROI</a>
+            <a href="support">Support</a>
           </nav>
         </header>
     );
